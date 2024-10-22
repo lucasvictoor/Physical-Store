d2675b055d42203d452ff6e3318fef5ddf355915
lucas8790 <lv8790121@gmail.com>
2024-10-22 09:40:51 -0300

    feat: conexão BD e inicialização servidor

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from '../db/conn';
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+connectDB();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
