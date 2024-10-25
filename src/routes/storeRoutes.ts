4776615cc70b23abe49e34565ffcfc3b4a739ba8
lucas8790 <lv8790121@gmail.com>
2024-10-25 13:19:56 -0300

    feat: rota para buscar lojas

--- src/routes/storeRoutes.ts
+++ src/routes/storeRoutes.ts
@@ -1,6 +1,6 @@
 // Rotas relacionadas às lojas físicas
 import { Router } from 'express';
-import { createStore, getStores, deleteStore, getStoreById } from '../controllers/storeController';
+import { createStore, getStores, deleteStore, getStoreById, buscarLojasProximas } from '../controllers/storeController';
 
 const router = Router();
 
@@ -12,5 +12,7 @@ router.get('/', getStores);
 router.delete('/:id', deleteStore);
 // Rota para obter uma loja por ID
 router.get('/:id', getStoreById);
+// Rota para buscar lojas próximas
+router.post('/stores/buscar-lojas', buscarLojasProximas);
 
 export default router;
